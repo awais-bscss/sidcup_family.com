828cee169bd0d479d10f7b5dc2c1ed4bd0da15fc
Muhammad Awais <kingyt6624@gmail.com>
2025-06-22 20:13:20 +0500

    track large files with LFS

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const curserBlur = document.querySelector("#curser-blur");
+const curser = document.querySelector("#curser");
+
+document.addEventListener("mousemove", function (event) {
+  const x = event.clientX;
+  const y = event.clientY;
+  curser.style.left = x + "px";
+  curser.style.top = y + "px";
+  curserBlur.style.left = x - 100 + "px";
+  curserBlur.style.top = y - 100 + "px";
+});
+
 gsap.to("#nav", {
   backgroundColor: "rgba(0, 0, 0, 0.8)",
   duration: 0.5,
@@ -9,6 +21,17 @@ gsap.to("#nav", {
     markers: true,
     start: "top -3%",
     end: "top -4%",
+    scrub: 2,
+  },
+});
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    markers: true,
+    start: "top -50%",
+    end: "top -100%",
     scrub: 2,
   },
 });
