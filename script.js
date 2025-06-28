9bcbc26fef8a1f58ffb74a448edb42652395c0c5
Muhammad Awais <kingyt6624@gmail.com>
2025-06-28 16:47:08 +0500

    completed Front-End project with animations

--- script.js
+++ script.js
@@ -9,16 +9,32 @@ document.addEventListener("mousemove", function (event) {
   curserBlur.style.left = x - 100 + "px";
   curserBlur.style.top = y - 100 + "px";
 });
+var h4s = document.querySelectorAll("h4");
+h4s.forEach((h4) => {
+  h4.addEventListener("mouseenter", function () {
+    curserBlur.style.transform = "(scale1.5)";
+    curser.style.border = "1px solid rgb(244, 249, 227)";
+    curser.style.transform = "scale(4)";
+    curser.style.backgroundColor = "transparent";
+    // curser.style.transition = "all 1s linear";
+  });
+  h4.addEventListener("mouseleave", function () {
+    curserBlur.style.transform = "scale(1)";
+    curser.style.border = "none";
+    curser.style.transform = "scale(1)";
+    curser.style.backgroundColor = "#91b508";
+  });
+});
 
 gsap.to("#nav", {
-  backgroundColor: "rgba(0, 0, 0, 0.8)",
+  backgroundColor: "#000",
   duration: 0.5,
   delay: 1,
   height: "15%",
   scrollTrigger: {
     trigger: "#nav",
     scroller: "body",
-    markers: true,
+    // markers: true,
     start: "top -3%",
     end: "top -4%",
     scrub: 2,
@@ -29,9 +45,82 @@ gsap.to("#main", {
   scrollTrigger: {
     trigger: "#main",
     scroller: "body",
-    markers: true,
+    // markers: true,
     start: "top -50%",
     end: "top -100%",
+    scrub: 2,
+  },
+});
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("#about .img_1, .img_2, #about-text", {
+  y: 90,
+  opacity: 0,
+  duration: 1,
+  // stagger: 0.4,
+  scrollTrigger: {
+    trigger: "#about",
+    scroller: "body",
+    start: "top 60%",
+    end: "top 55%",
+    toggleActions: "restart none restart none",
+    scrub: 2,
+  },
+});
+gsap.from(".card", {
+  y: 90,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.3,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 55%",
+    toggleActions: "restart none restart none",
+    scrub: 2,
+  },
+});
+gsap.from("#colon1", {
+  y: -50,
+  x: -50,
+  // opacity: 0,
+  duration: 1,
+  // stagger: 0.3,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    start: "top 20%",
+    end: "top 15%",
+    toggleActions: "restart none restart none",
+    scrub: 4,
+  },
+});
+gsap.from("#colon2", {
+  y: 50,
+  x: 50,
+  // opacity: 0,
+  duration: 1,
+  // stagger: 0.3,
+  scrollTrigger: {
+    trigger: "#colon2",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 72%",
+    toggleActions: "restart none restart none",
+    scrub: 2,
+  },
+});
+gsap.from("#page_4 h1", {
+  y: 30,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#page_4 h1",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 65%",
+    toggleActions: "restart none restart none",
     scrub: 2,
   },
 });
